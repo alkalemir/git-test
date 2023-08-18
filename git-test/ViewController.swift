698826fed97bd3986525d11b46dfed518a744391
Emir Alkal <alkalemirdev@gmail.com>
2023-08-18 15:39:50 +0300

    Initial Commit

---
+++ git-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  git-test
+//
+//  Created by Emir Alkal on 18.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
